ee72ee1016f6fa9dbb26f6739858cd3f2c493e05
Dmitrii <kuzneczovdv@gmail.com>
2024-01-07 08:06:50 +0300

    Напишите программу, которая принимает на вход // трёхзначное целое число и на выходе показывает сумму первой и последней цифры этого числа.

---
+++ 1_1/1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       Console.Write("Введите число a= : ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 7 == 0 && number % 23 == 0)
+        {
+            Console.WriteLine($"a={number}   => да");
+        }
+        else
+        {
+            Console.WriteLine($"a={number}   => нет");
+        }
+    }
+}
+
