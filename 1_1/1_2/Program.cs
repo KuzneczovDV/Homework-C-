c6b03a88ff6c25ad2f62f4ef669a5cb91c588c2a
Dmitrii <kuzneczovdv@gmail.com>
2024-01-07 08:21:57 +0300

    Задача № 2

---
+++ 1_1/1_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       Console.WriteLine("Введите координаты X и Y через запятую с пробелом:");
+       string[]coordinates = Console.ReadLine().Split(',');
+       int x = Convert.ToInt32(coordinates[0]);
+       int y = Convert.ToInt32(coordinates[1]);
+
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine("=> 1");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("=> 2");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("=> 3");
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine("=> 4");
+        }
+        else
+        {
+            Console.WriteLine("Точка лежит на одной из координатных осей.");
+        }
+    }
+}
+
