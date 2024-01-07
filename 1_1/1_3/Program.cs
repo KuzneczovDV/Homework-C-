01c62e00d6dcaefa4507114a5fab24029733d410
Dmitrii <kuzneczovdv@gmail.com>
2024-01-07 08:34:23 +0300

    Задача № 3

---
+++ 1_1/1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число от [10, 99]:");
+        int number = int.Parse(Console.ReadLine());
+
+        int firstDigit = number / 10;
+        int secondDigit = number % 10;
+        
+        int maxDigit = Math.Max(firstDigit, secondDigit);
+
+        Console.WriteLine($"{number}  => {maxDigit}");
+    }
+}
