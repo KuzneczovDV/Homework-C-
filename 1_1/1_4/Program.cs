c20a7c4f2c6f6b882f7b6d40b3b2417775ba87ed
Dmitrii <kuzneczovdv@gmail.com>
2024-01-07 08:39:30 +0300

    Задача № 4

---
+++ 1_1/1_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите натуральное число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n <= 0)
+        {
+            Console.WriteLine("Введите положительное натуральное число.");
+            return;
+        }
+
+        Console.WriteLine($"{n}  => {string.Join(", ", GetDigits(n))}");
+    }
+
+    static int[] GetDigits(int number)
+    {
+        char[] charArray = number.ToString().ToCharArray();
+        int[] digits = Array.ConvertAll(charArray, c => (int)Char.GetNumericValue(c));
+        return digits;
+    }
+}
